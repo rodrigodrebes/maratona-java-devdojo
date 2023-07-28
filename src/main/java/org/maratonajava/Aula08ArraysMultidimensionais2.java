ea850c02d00922f120cf59f9e32cd15549f709e2
Rodrigo <rodrigodrebes97@gmail.com>
2023-07-28 16:23:08 -0300

    Arrays Multidimensionais

---
+++ src/main/java/org/maratonajava/Aula08ArraysMultidimensionais2.java
@@ -0,0 +1,18 @@
+package org.maratonajava;
+
+public class Aula08ArraysMultidimensionais2 {
+    public static void main(String[] args) {
+        int[][] arrayInt = new int [3][];
+
+        arrayInt[0] = new int[2];
+        arrayInt[1] = new int [3];
+        arrayInt[2] = new int [6];
+
+        for(int[] arrayBase: arrayInt){
+            System.out.println("\n----------");
+            for(int num: arrayBase){
+                System.out.println(num + " ");
+            }
+        }
+    }
+}
