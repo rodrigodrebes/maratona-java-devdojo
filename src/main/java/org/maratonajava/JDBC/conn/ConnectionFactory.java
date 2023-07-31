56b9d65f8c9e06fcb4e7c40f3a49557a1686db44
Rodrigo <rodrigodrebes97@gmail.com>
2023-07-31 15:49:36 -0300

    JDBC getConnection + docker

---
+++ src/main/java/org/maratonajava/JDBC/conn/ConnectionFactory.java
@@ -0,0 +1,31 @@
+package org.maratonajava.JDBC.conn;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+    //java.sql = Connection, Statement, ResultSet, DriverManager
+
+    // Primeiro passo: criar a conexão
+    public static Connection getConnection(){
+
+        String url = "jdbc:mysql://localhost:3308/anime_store";
+        String username = "root";
+        String password = "root";
+
+        try {
+            Connection connection = DriverManager.getConnection(url, username,password);
+
+            System.out.println(connection);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return null;
+
+    }
+
+
+}
---
+++ src/main/java/org/maratonajava/JDBC/test/ConnectionFactoryTest01.java
@@ -0,0 +1,10 @@
+package org.maratonajava.JDBC.test;
+
+import org.maratonajava.JDBC.conn.ConnectionFactory;
+
+public class ConnectionFactoryTest01 {
+
+    public static void main(String[] args) {
+        ConnectionFactory.getConnection();
+    }
+}
